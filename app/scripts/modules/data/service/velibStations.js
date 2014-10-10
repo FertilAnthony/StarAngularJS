2ee3ae70a8cf5efcc210bf219f1a8893d9cdc522
Anthony FERTIL <anthony.fertil@niji.fr>
2014-10-10 18:03:44 +0200

    Affichage de la liste des stations de velib

--- app/scripts/modules/data/service/velibStations.js
+++ app/scripts/modules/data/service/velibStations.js
@@ -3,6 +3,28 @@
 /**
  * @ngInject
  */
- function StationsService($http, $log) {
+function StationsService($http, $log, OpenDataApi) {
+  // Interface
+  var service = {
+    get: get
+  };
+  return service;
 
- }
+  // Implementation
+
+  function get() {
+    var apiArgs = {
+      url: OpenDataApi.url,
+      method: 'GET',
+      params: {
+        version: OpenDataApi.version,
+        key: OpenDataApi.key,
+        cmd: 'getbikestations' // A modifier pour les bus après
+      }
+    };
+    // TODO: Appel au web service en utilisant le service angular $http
+    return $http(apiArgs);
+  }
+}
+
+module.exports = StationsService;
--- app/scripts/modules/ui/controllers/home.js
+++ app/scripts/modules/ui/controllers/home.js
@@ -3,18 +3,18 @@
 /**
  * @ngInject
  */
-function HomeController() {
+function HomeController(Stations, $log) {
   // ViewModel
   var vm = this;
 
   vm.welcomeMessage = 'Bienvenue dans l\'application pour la star de Rennes';
   vm.stations = [];
 
-  /*Stations.get().then(function onSuccess(response) {
+  Stations.get().then(function onSuccess(response) {
   	var stations = response.data.opendata.answer.data.station;
   	vm.stations = stations;
   	$log.log(stations);
-  });*/
+  });
 
 }
 
